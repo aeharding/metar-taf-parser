aca5c4badce432f5eba7969b1d39cb9e8e9f30ef
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-04-09 23:46:33 -0500

    Fix test

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export interface IMetarTAFParserOptions {
 
 export function parseMetar(
   metar: string,
-  options: IMetarTAFParserOptions
+  options?: IMetarTAFParserOptions
 ): IMetar {
   const lang = options?.locale || en;
 
