7174827fe2ffcb94feefd25a6069f0940220fd8d
Alexander Harding <noreply@harding.dev>
2025-03-01 11:38:41 -0600

    (chore) remove corepack, upgrade build dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -23,9 +23,10 @@ const globalStyles = css`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", sans-serif;
+    font-family:
+      -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 
@@ -54,8 +55,8 @@ const globalStyles = css`
   }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
+    font-family:
+      source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
   }
 `;
 
